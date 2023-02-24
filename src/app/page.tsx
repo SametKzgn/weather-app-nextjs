7f5ced492e13d03e230baaea9e963115d37745fd
Samet Kızgın <samet.kizgin@gunsel.local>
2023-02-24 11:04:44 +0200

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,137 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import {useState} from "react";
+import "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const API_KEY = "dff6dd2ff97754a80272c82885e9cc78";
+
+// sam's first next js project - weather app for Beyza <3
+
+// localhost:3000 - home page
+// we are going to design the user interface
+//  then we are going to call the openweather API
+// show the user data based pff the API result (seatle -> seatle weather openweather API)
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const [cityInput, setCityInput] = useState({}); //seattle
+
+    const [weatherData, setWeatherData] = useState<any>({}); //weather data
+
+    async function getWeatherData() {
+        console.log("button");
+        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        //queyr data
+        //if there is an error, throw error
+        //if not save data
+        try {
+            const serverResponse = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?` +
+                "q=" +
+                cityInput +
+                "&appid=" +
+                API_KEY +
+                "&units=imperial"
+            );
+            const data = await serverResponse.json();
+            console.log(data);
+            if (data.cod === "404") throw data;
+            setWeatherData(data);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    console.log(cityInput);
+
+    return (
+        // Normalde tailwind css kulanacaktım fakat next js in css kütüphanesi ile devam edeceğim
+        <div
+            style={{
+                backgroundImage: "url('https://littlevisuals.co/images/fresh_cut.jpg')",
+                position: "static",
+                height: "100vh",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundAttachment: "fixed",
+                color: "black",
+                fontFamily: "Inter",
+                fontSize: "1.5rem",
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "1rem",
+            }}
+        >
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                width: "100%",
+                maxWidth: "40rem",
+                height: "100%",
+                maxHeight: "20rem",
+                backgroundColor: "rgba(217,212,212,0.7)",
+                padding: "1rem",
+                borderRadius: "1rem",
+                gap: "1rem",
+            }}>
+                <div>
+                    <h2 style={{fontWeight: "600"}}>
+                        Get The Weather!
+                    </h2>
+                    <p>Enter a city name, and get the weather!</p>
+                </div>
+
+                <div style={{display: "flex", justifyContent: "center", gap: "1rem"}}>
+                    <input
+                        onChange={(e) => {
+                            setCityInput(e.target.value)
+                        }} //c -> setCityInput("C")
+                        style={{
+                            width: "20rem",
+                            height: "3rem",
+                            padding: "0.5rem",
+                            borderRadius: "0.6rem",
+                            border: "none",
+                            outline: "none",
+                        }} type="text" placeholder="Enter a city name"/>
+                    <button
+                        onClick={() => getWeatherData()}
+                        style={{
+                            width: "6rem",
+                            height: "3rem",
+                            padding: "0.5rem",
+                            borderRadius: "0.6rem",
+                            border: "none",
+                            outline: "none",
+                            backgroundColor: "rgba(0,0,0,0.7)",
+                            color: "white",
+                            cursor: "pointer"
+                        }}>Get Weather
+                    </button>
+                </div>
+
+                <>
+                    <div>
+                        <h3>{weatherData?.name} Weather</h3>
+                        <h3>
+                            Currently{weatherData?.main?.temp} &deg;F
+                        </h3>
+                        {weatherData?.weather?.length > 0 ?
+                            <img
+                                src={"https://openweathermap.org/img/wn/" + weatherData?.weather[0]?.icon + "@4x.png"}
+                                alt=""/> : null
+                        }
+                    </div>
+                </>
+
+
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
